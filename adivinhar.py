f043194b68eba5a317094a54333429dab9531b60
Sérgio Renato Steglich <79202041+srsteglich@users.noreply.github.com>
2023-07-12 14:52:53 -0300

    Atualizando os jogos
    Jogo de Adivinhação de Números, com a pontuação.

--- adivinhar.py
+++ adivinhar.py
@@ -1,7 +1,9 @@
 import random
 
 def jogar():
-    print("\n       Bem-vindo ao jogo de adivinhação de números!\n")  
+    print("-------------------------------------------------------------------------------")
+    print("       Bem-vindo ao jogo de adivinhação de números, com pontuação do jogo!")  
+    print("-------------------------------------------------------------------------------")    
     nivel = 0   
     ponto = 0    
     while nivel != 4:       
@@ -10,31 +12,33 @@ def jogar():
         print("  2 - Médio (10 Tentativas)")
         print("  3 - Difícil (05 Tentativas)")
         print("  4 - Sair do Jogo")
-        nivel = int(input("  Digite um número:"))              
+        nivel = int(input("  Digite um número para nível de dificuldade: "))              
         if nivel == 1:
-            ponto = 10                                
+            ponto = 5
+            vezes = 20                                
             break
         elif nivel == 2:
-            ponto = 30              
+            ponto = 10
+            vezes = 10                                              
             break            
         elif nivel == 3:
-            ponto = 80            
+            ponto = 30            
+            vezes = 5                                
             break
         elif nivel == 4:          
             exit(" \n  Saiu...\n")  
         else: 
-           print("\n  Opcao invalida....\n")  
+           print("\n  Opção inválida....\n")  
            #continue  
-        return jogar()      
-            
+        return jogar()                  
     ## print(" quanto ponto", ponto)
-    print("\n  Escolha os limites do Jogo, dê diferença até 50 números ")    
+    print("\n  Escolha os limites do Jogo, ideal é a diferença até 50 números. ")    
     while True:    
         infer = int(input("  Digite o limite inferior: "))
         super = int(input("  Digite o limite superior: "))                          
         quant = super - infer       
         if 2 <= quant <= 9:
-            ## print("estou linha 37")
+            ## print("estou linha 41")
             ponto = ponto + 5   
             break            
         elif 10 <= quant <= 19:
@@ -44,39 +48,55 @@ def jogar():
             ponto = ponto + 20
             break               
         elif 30 <= quant <= 39:
-            ponto = ponto + 40            
+            ponto = ponto + 30            
             break              
         elif 40 <= quant <= 50:
-            ## print("estou linha 50")
-            ponto = ponto + 80            
+            ## print("estou linha 54")
+            ponto = ponto + 50            
             break               
         else:
             print("\n  Valor invalido.") 
             print("  O limite superior não pode ser menor ou igual que limite inferior....")                           
         continue                                
-    ## print(" ponto 2 linha 57", ponto)
-    numCPU = random.randint(infer,super)
-    print(" o numero do CPU " , numCPU)
-    vezes = 0
-    
-    while True:
-        plpt = int(input("  Digite o seu palpite: "))   
-        vezes += 1
-        #### falta pontos e correto o numero CPU
-        
+    ## print(" ponto2 linha 57", ponto)    
+    print("\n  Os números escolhido foi",infer,"à",super,".")
+    print("  E tem",vezes,"tentativas.\n")
+    numCPU = random.randint(infer,super)    
+    for vez in range(1, vezes + 1):
+        print("\n  Tentativa número:",vez)
+        plpt = int(input("  Digite o seu palpite: "))           
         if plpt < numCPU:
-            print("  Tente um número maior!")
-        elif plpt < numCPU:
-            print("  Tente um número menor!")
+            print("\n  Tente um número maior!")
+            ponto = ponto - 10
+        elif plpt > numCPU:
+            print("\n  Tente um número menor!")
+            ponto = ponto - 10
         else:
-            print(f"  Uuuuhhhhh.... Você acertou o número {numCPU} e foram {vezes} vezes.\n")
-
-            resp = input("Deseja jogar novamente[S/N]?").upper()
-            if resp == 'S':      
-                jogar()
-            else:    
-                print(" Encerrou") 
-                exit()
+            break        
+    ##print("  vezes:",vezes)     
+    ##print("  vez:",vez)         
+    ##print("  pontuacao:",ponto)   
+    if plpt == numCPU:
+        print("\n  Uuuuhhhhh.... Você acertou o número ",numCPU," e foram ",vez,"vezes.")        
+        print("  E a tua Pontuação do Jogo foi: ",ponto,"pontos.\n")
+        if ponto >= 50:
+            print("  A tua Pontuação do Jogo foi Muito Excelente!!!!! Parabéns...\n")
+        else:    
+            print("  A tua Pontuação do Jogo foi Bom!!!!! Parabéns...\n")            
+    else:
+        print("\n  Não foi desta vez.... O Número Secreto era",numCPU,".\n")
+        print("  E a tua Pontuação do Jogo foi:",ponto,"pontos.\n")
+        if ponto >= 5:
+            print("  A tua Pontuação do Jogo foi Razoável...\n")
+        else:    
+            print("  A tua Pontuação do Jogo foi muito Baixo...\n")  
+    resp = input("  Deseja Jogar novamente[S/N]?").upper()
+    if resp == 'S':      
+        jogar()
+    else:    
+        print("\n  Encerrado, até próxima!!!\n") 
+    exit()    
+        
 jogar()    
     
     
